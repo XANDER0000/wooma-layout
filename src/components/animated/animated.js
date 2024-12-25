848800082f37318d95ce3d769818b509bbdbc453
XANDER0000 <guznyaev04@gmail.com>
2024-12-26 01:24:58 +0300

    add animations by scroll

---
+++ src/components/animated/animated.js
@@ -0,0 +1,34 @@
+const animatedItems = document.querySelectorAll('.animated-item');
+
+const applyAnimation = (item) => {
+  const settings = JSON.parse(item.getAttribute('data-settings'));
+  if (settings && settings.animation) {
+    const animationClass = settings.animation;
+    const animationDelay = settings.animationDelay || 0;
+    const animationDuration = settings.animationDuration || 0;
+
+    if (animationDelay !== 0) item.style.animationDelay = `${animationDelay}ms`;
+    if (animationDuration !== 0) item.style.animationDuration = `${animationDuration}ms`;
+    item.classList.add(animationClass);
+
+    const animationEndHandler = () => {
+      item.classList.remove(animationClass);
+      item.classList.add('animation-complete');
+      item.removeEventListener('animationend', animationEndHandler);
+    };
+    item.addEventListener('animationend', animationEndHandler);
+  }
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting && !entry.target.classList.contains('animation-complete')) {
+      applyAnimation(entry.target);
+      entry.target.classList.add('animated');
+    }
+  });
+}, {
+  threshold: 0.1,
+});
+
+animatedItems.forEach((item) => observer.observe(item));
